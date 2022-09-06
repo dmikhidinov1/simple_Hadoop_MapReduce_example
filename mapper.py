88fe20d4d1b73f89a40f058cf9caa35169c3989d
Dilyor Mikhidinov <desmondd4u@gmail.com>
2022-09-06 08:34:20 +0000

    added 2 more stopwords

--- mapper.py
+++ mapper.py
@@ -6,9 +6,17 @@ for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
 
+    # make lower case
+    line = line.lower()
+
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
+    stopwords = set(['the', 'and', 'a', 'for','I','go','four','three','be','not','to','railings','or'])
+
+    # notice the loop is different
+
     for word in words:
+	if word not in stopwords
         print '%s\t%s' % (word, "1")
